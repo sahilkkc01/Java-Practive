adc54f3f3343c74c8748c597456818cf8f485572
sahilkkc01 <sahilgupta5655@gmail.com>
2024-03-29 12:34:27 +0530

    Java practice

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void printjava(){
+        System.out.println("HEllo java");
+    }
+
+    public static void Sum(int a,int b){
+        System.out.println(a+b);
+    }
+
+    public static void main(String[] args) {
+//        System.out.println("Aryan");
+//        System.out.println("sand");
+//        byte age=10;
+//        System.out.println(age);
+//        long phone=309840329;
+//        System.out.println(phone);
+//        Float pi=3.15F;
+
+//        String name="aryan";
+//        String name2="rajan";
+//        String name3=name+name2;
+//        System.out.println(name3);
+//
+//        String namex=name.replace('a','b');
+//        System.out.println(namex);
+//
+//        int[] marks= new int[3];
+//        marks[0]=12;
+//        marks[1]=11;
+//        marks[2]=10;
+//        System.out.println(marks[0]);
+//        Arrays.sort(marks);
+//        System.out.println(marks[0]);
+
+//        float pi=3.14F;
+//        System.out.println(pi);
+
+//        Scanner sc= new Scanner(System.in);
+//        int age=sc.nextInt();
+//        System.out.println(age);
+//        for(int i=0;i<=10;i++){
+//            System.out.println(i);
+//        }
+
+
+        //Exception Handeling
+
+//        int marks[]={97,98,91};
+//        try{
+//            System.out.println(marks[5]);
+//        }catch (Exception exception){
+//            System.out.println("Invalid idx");
+//        }
+//        System.out.println("Name of student is sahil");
+//
+
+        printjava();
+        Sum(5,6);
+    }
+}
